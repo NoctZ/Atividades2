0da035f5d77fb7d7bef79a545857112af2031c20
NoctZ <111011473+NoctZ@users.noreply.github.com>
2022-08-30 16:59:58 -0300

    Initial commit

---
+++ Antigos/Curso js/Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 90.5
+console.log(`A velocida de do teu carro é ${vel}km/h`)
+if (vel > 60) { 
+    console.log (`você ultrapassou a velocidade permitida, MULTADO!`)
+
+
+}
+console.log(`dirija sempre com cinto de segurança`)
---
+++ projeto/30praum.js
@@ -0,0 +1,5 @@
+var codigo = executar
+console.log(`Estamos ${codigo} `)
+function digitar() {
+    codigo
+}
